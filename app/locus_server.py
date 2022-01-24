a29e4e7c96ff656e54d8a3c292efd6e4451b56da
Dan Lenehan <dan@myurbanjungle.com>
2022-01-24 13:42:16 +0000

    Open RMQ connection

--- app/locus_server.py
+++ app/locus_server.py
@@ -1,14 +1,27 @@
 from flask import Flask, request
 from flask_cors import CORS
+import json
+import pika
 
 app = Flask(__name__)
 CORS(app)
 
 
+
 @app.route("/", methods=["POST"])
 def hello():
-    print("Request", request.data.decode())
+    user_info = request.data.decode()
+    print("Request", user_info)
+    channel.queue_declare(queue='harrier')
+    channel.basic_publish(
+        exchange='',
+        routing_key='harrier',
+        body=user_info
+    )
     return {"message": "data received"}
 
 if __name__ == "__main__":
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
     app.run(host='0.0.0.0')
+    #channel.close()
