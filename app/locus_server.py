953e9253873644f1de38904aa14f6fe9b1ba10c4
Dan Lenehan <dan@192.168.1.101>
2022-04-16 15:46:59 +0100

    Recent Changes

--- app/locus_server.py
+++ app/locus_server.py
@@ -1,12 +1,19 @@
 from email import message
 from flask import Flask, request
 from flask_cors import CORS
+from flask import session
+from flask_session import Session
+import redis
 import json
 import pika
 
 app = Flask(__name__)
+SECRET_KEY = '123456789012345678901234'
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_REDIS'] = redis.Redis()
+app.config['SECRET_KEY'] = SECRET_KEY
 CORS(app)
-
+Session(app)
 
 
 @app.route("/", methods=["POST"])
@@ -21,9 +28,14 @@ def hello():
         routing_key='harrier',
         body=json.dumps(message)
     )
+    session['key'] = 123
+
+    print('Key: ', session.get('key'))
+
     return {"message": "data received"}
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    app.run(host='0.0.0.0', port=5001)
+
+    app.run(host='0.0.0.0', port=5002)
